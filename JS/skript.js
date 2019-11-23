935fe319f1e1123e7095a388135ece0a9a0d885b
pekolinac <o.biljana@gmail.com>
2019-11-24 00:51:37 +0100

    domaci zadatak-js

--- JS/skript.js
+++ JS/skript.js
@@ -1,9 +1,18 @@
-function racunaj(a, b){
+function racunaj(a, b) {
     var c = 5;
     var rezultat = (a + b) * c;
-    return(rezultat);
-    
+    return rezultat;   
 }
-function otvori(){
-    window.
+var rezultatFunkcije = racunaj(4, 6);
+   console.log("Rezultat je " + rezultatFunkcije);
+var prozor;
+function zatvori() {
+    prozor.close();
 }
+
+function otvori() {
+    window.open("zadatak-js/reklama.html", "_blank", "width=320, height= 250");
+}
+
+setTimeout('zatvori()', 6000);
+
