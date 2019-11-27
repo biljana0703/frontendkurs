c6bcd436332cc831f0c66389c9ead618ca001b3e
pekolinac <o.biljana@gmail.com>
2019-11-27 20:06:17 +0100

    novi zadatak jquery

---
+++ jQuery/jQuery-Zadatak1/zadatak-jq/skriptjq.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('body').css({'background-color':'burlywood', 'font-family':'Tahoma'}); 
+  $('form').addClass('stilForme')
+  $('#sakrij').click(function(){
+    $('#kontakt').hide(1500);
+    })
+  $('#slika').prepend('<img src="ns.jpg" style="display:block;">');
+  $('#prikazi').click(function(){
+      $('#kontakt').toggle(1500);
+  })
+    
+})
